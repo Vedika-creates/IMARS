b316861fb5192418051ffcca4ba8519b6013efa4
Vedika-creates <vedikak1312@gmail.com>
2026-01-09 21:17:29 +0530

    Your commit message here

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ dotenv.config();
 const app = express();
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:5173", "https://imars1.vercel.app"],
+    origin: ["http://localhost:5000","http://localhost:5173", "https://imars1.vercel.app"],
     credentials: true
   })
 );
